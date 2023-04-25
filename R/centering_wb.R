bd1daf5cb1a9ac4afff5dc892a5e859dd4654ea7
Pascal M. Küng <pascal.kueng@psychologie.uzh.ch>
2023-04-25 22:00:03 +0200

    now weighting the between correlation correctly

--- R/centering_wb.R
+++ R/centering_wb.R
@@ -11,7 +11,7 @@ wbCenter <- function(input_data, cluster) {
   } else if (ncol(input_data) < 2) {
     stop("input_data must have at least two columns")
   }
-  
+
   # Determine Cluster Variable
   if (is.character(cluster)) {
     if (!cluster %in% colnames(input_data)) {
@@ -23,32 +23,35 @@ wbCenter <- function(input_data, cluster) {
     cluster_var <- cluster
     cluster <- "cluster"
   }
-  
+
   # Center the data
   df_within <- data.frame(cluster = cluster_var)
   df_between <- data.frame(cluster = cluster_var)
-  
+
   for (name in colnames(input_data)) {
     col <- input_data[[name]]
-    
+
     # Drop all non-numeric columns
     if(!is.numeric(col)) {
       warning(paste("CAUTION: Non-Numeric Variable", name ,"was set to NA!
       For correlations involving factors consider psych::statsBy"))
       col <- NA
     }
-    
+
     grand_mean <- mean(col, na.rm = TRUE)
     col_grand_mean_c <- col - grand_mean
-    
+
     # Centering between clusters
     between <- suppressWarnings(ave(col_grand_mean_c, cluster_var, FUN = function(x) mean(x, na.rm = TRUE)))
     df_between[[name]] <- between
-    
+
     # Centering within clusters
     within <- col_grand_mean_c - between
     df_within[[name]] <- within
+
+    # When there was an NA we set NA in the between centered Variable too to make sure we have a weighted score in the end.
+    df_between[is.na(df_within[name]), name] <- NA
   }
-  
+
   return(list(between = df_between, within = df_within))
 }
