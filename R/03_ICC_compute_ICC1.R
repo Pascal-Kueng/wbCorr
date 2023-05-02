e842b0e39cfb7a643ef9871e414f345bfef777f2
Pascal M. Küng <pascal.kueng@psychologie.uzh.ch>
2023-05-02 18:40:39 +0200

    probably all working :D

--- R/03_ICC_compute_ICC1.R
+++ R/03_ICC_compute_ICC1.R
@@ -1,6 +1,6 @@
-#' @importFrom nlme lme
+#' @importFrom nlme lme VarCorr
+#' @importFrom stats as.formula
 compute_ICC1 <- function(input_data, cluster_var, cluster_name) {
-  library('nlme')
 
   if (!(cluster_name %in% colnames(input_data))) {
     input_data[[cluster_name]] <- cluster_var
@@ -21,14 +21,22 @@ compute_ICC1 <- function(input_data, cluster_var, cluster_name) {
     formula <- as.formula(paste0("`", name, "` ~ 1"))
     random_formula <- as.formula(paste0("~ 1 | `", cluster_name, "`"))
 
-    model <- suppressMessages(suppressWarnings(
-      lme(fixed = formula, data = input_data, random = random_formula, na.action = 'na.omit')))
+    ICC <- NA
+    tryCatch({
+      model <- suppressMessages(suppressWarnings(
+        nlme::lme(fixed = formula, data = input_data, random = random_formula, na.action = 'na.omit')))
 
-    vc <- VarCorr(model)
-    between_variance <- as.numeric(vc[1,1]) # extract variance
-    residual_variance <- as.numeric(vc[2,1]) # extract SD and square for var.
+      vc <- VarCorr(model)
+      between_variance <- as.numeric(vc[1,1]) # extract variance
+      residual_variance <- as.numeric(vc[2,1]) # extract SD and square for var.
 
-    ICC <- round(between_variance / (between_variance + residual_variance),2)
+      ICC <- round(between_variance / (between_variance + residual_variance),2)
+
+    }, error = function (e) {
+      warning(e)
+      ICC <- NA
+    })
+
 
     ICCs <- rbind(ICCs, data.frame(variable = name, ICC = ICC)) # Add the result to the data frame
   }
